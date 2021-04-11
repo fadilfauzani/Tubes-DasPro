40a4c7e445992f8b495ada77260f97fc7f7e4554
fadilfauzani <16520180@mahasiswa.itb.ac.id>
2021-04-11 22:30:48 +0700

    pengganti split()

---
+++ source/stringtodata.py
@@ -0,0 +1,16 @@
+def stringtodata(s):
+    #s adalah string yang ingin diubah menjadi data
+    #dipakai untuk menggantikan fungsi split
+    datas = []
+    i = 0
+    data = ""
+    while (i < len(s)):
+        if (s[i]==','):
+            datas.append(data)
+            data = ""
+        else:
+            data += s[i]
+            if (i == len(s)-1):
+                datas.append(data)
+        i += 1
+    return datas
