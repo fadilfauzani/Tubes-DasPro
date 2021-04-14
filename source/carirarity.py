32ed7a9cb4ef69962397ba1688bc81846131d3e5
Brandonjonathan <brandonjonathan7320@gmail.com>
2021-04-14 19:51:39 +0700

    Enter

---
+++ source/carirarity.py
@@ -0,0 +1,14 @@
+def carirarity() :
+    rarity = input("Masukan rarity : ")
+    print()
+    print ("Hasil pencarian : ")
+
+    for i in range (len(gadgets)) :
+        if rarity == gadgets[i][4] :
+            print()
+            print("Nama            : " + gadgets[i][1])
+            print("Deskripsi       : " + gadgets[i][2])
+            print("Jumlah          : " + gadgets[i][3])
+            print("Rarity          : " + gadgets[i][4])
+            print("Tahun Ditemukan : " + gadgets[i][5])
+carirarity()
