7c10595468fc67da274591aa25c50731e5c1ebd5
fadilfauzani <16520180@mahasiswa.itb.ac.id>
2021-04-14 12:58:09 +0700

    tambahitem + hapusteim

--- source/tambahitem.py
+++ source/tambahitem.py
@@ -1,12 +1,27 @@
 gadgets = [['G1']]
 consums = []
 
+def idxID(id):
+    global gadgets, consums
+    idx = -1
+    if (id[0] == 'G'):
+        for i in range(len(gadgets)):
+            if (id == gadgets[i][0]):
+                idx = i
+    else:
+        for i in range(len(consums)):
+            if (id == consums[i][0]):
+                idx = i
+    return idx
+
+    
+
 def idValid(id):    #skema validasi input id
     if (id[0] == 'G' or id[0] == 'C'):
         return True
     else:
         return False
-def idSudahada(id):
+def idada(id):
     if (id[0] == 'G'):
         for i in gadgets:
             if (id == i[0]):
@@ -28,7 +43,7 @@ def tambahitem():
     global gadgets, consums
     id = input("Masukan ID: ")
     if(idValid(id)):
-        if (idSudahada(id)):
+        if (idada(id)):
             print("\nGagal menambahkan item karena ID sudah ada")
         else:
             nama = input("Masukan Nama: ")
@@ -46,4 +61,20 @@ def tambahitem():
                 print("\nInput rarity tidak valid!")
     else:
         print("\nGagal menambahkan item karena ID tidak valid")
+def hapusitem():
+    global gadgets, consums
+    id = input("Masukan ID item: ")
+    if (idada(id)):
+        hapus = input("Apakah anda yakin ingin menghapus Pintu ke ITB (Y/N)?")
+        if (hapus):
+            if (id[0] == 'G'):
+                gadgets.pop(idxID(id))
+            else:
+                consums.pop(idxID(id))
+
+    else:
+        print("Tidak ada item dengan ID tersebut")
 tambahitem()
+print(gadgets)
+hapusitem()
+print(gadgets)
