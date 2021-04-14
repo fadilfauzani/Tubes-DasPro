f8d63cb4ff5f87b5ca98eab9be6dde577223fd34
Brandonjonathan <brandonjonathan7320@gmail.com>
2021-04-14 19:09:56 +0700

    Enter

---
+++ source/login.py
@@ -0,0 +1,14 @@
+def login() :
+    akses = False
+
+    username = input("Masukan username : ")
+    password = input("Masukan password : ")
+
+    for i in range (len(users)) :
+        if username == users[i][1] and password == users[i][4] :
+            akses = True
+
+    if akses : 
+        print ("Halo " + username + "! Selamat datang di Kantong Ajaib.")
+    else :
+        print ("Username atau password salah, silahkan coba lagi.")
